328a6f5d08b970bb2c6328f19dbc58b82ccfbfa2
Meishan <meishan.lin@aalto.fi>
2024-11-16 12:37:09 +0200

    to csv

--- 1_change_to_csv.r
+++ 1_change_to_csv.r
@@ -1,6 +1,6 @@
 library(readxl)
 input_file <- "/Users/meishanlin/BDA_project/BDA_project/Price History_20241108_1323.xlsx" 
-output_file <- "/Users/meishanlin/BDA_project/BDA_project/SP500_History.csv.gz"
+output_file <- "/Users/meishanlin/BDA_project/BDA_project/SP500_History.csv"
 data <- read_excel(input_file, skip = 17)  
 write.csv(data, gzfile(output_file), row.names = FALSE)
 cat("Filtered data saved to:", output_file, "\n")
